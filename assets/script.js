76a987b0216099a69d1f5a83a6812b52b849d4cc
Isabel Yang <isabelyang@berkeley.edu>
2015-01-18 22:29:07 -0800

    Front page buttons

---
+++ assets/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+
+
+$("#aboutme-image").hover(function(){
+    $(this).stop().fadeTo(400, 0);
+}, function(){
+    $(this).stop().fadeTo(300, 1);
+});
+
+$("#pastwork-image").hover(function(){
+    $(this).stop().fadeTo(400, 0);
+}, function(){
+    $(this).stop().fadeTo(300, 1);
+});
+
+$("#resume-image").hover(function(){
+    $(this).stop().fadeTo(400, 0);
+}, function(){
+    $(this).stop().fadeTo(300, 1);
+});
+
+$("#contact-image").hover(function(){
+    $(this).stop().fadeTo(400, 0);
+}, function(){
+    $(this).stop().fadeTo(300, 1);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+})
